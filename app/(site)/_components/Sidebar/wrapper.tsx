8e3a58bd9f91e3d670c4e5d75040bb04d5e13c7b
DuarteMartinho <duartemartinho11@gmail.com>
2024-01-14 20:10:14 +0000

    12-1: fixed bug in last commit

--- app/(site)/_components/Sidebar/wrapper.tsx
+++ app/(site)/_components/Sidebar/wrapper.tsx
@@ -5,6 +5,7 @@ import { useSidebar } from "@/store/use-sidebar";
 import { useEffect, useState } from "react";
 import { ToggleSkeleton } from "./Toggle";
 import { RecommendationsSkeleton } from "./Recommendations";
+import { useIsClient } from "usehooks-ts";
 
 interface WrapperProps {
     children: React.ReactNode;
@@ -13,12 +14,8 @@ interface WrapperProps {
 const Wrapper = ({
     children
 }: WrapperProps) => {
-    const [isClient, setIsClient] = useState(false);
+    const isClient = useIsClient();
     const { collapsed } = useSidebar((state) => state);
-
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
 
     if (!isClient) {
         return (
@@ -35,7 +32,7 @@ const Wrapper = ({
     return (
         <aside
             className={cn(
-                "fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50",
+                "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
                 collapsed && "w-[70px]"
             )}
         >
