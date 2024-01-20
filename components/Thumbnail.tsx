4202f07ce12cfc71709bc267067342c0baecac2b
DuarteMartinho <duartemartinho11@gmail.com>
2024-01-20 18:11:17 +0000

    32: fixed little bugs

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { UserAvatar } from "./UserAvatar";
 import { Skeleton } from "./ui/skeleton";
+import { LiveBadge } from "./live-badge";
 
 interface ThumbnailProps {
     src: string | null;
@@ -15,7 +16,7 @@ export const Thumbnail = ({ src, fallback, isLive, username }: ThumbnailProps) =
     if (!src) {
         content = (
             <div
-                className="flex flex-col items-center justify-center bg-background gap-y-4 h-full w-full transition-transform group-hover:translate-x-2 group-hover:-translate-y-1 rounded-md"
+                className="flex flex-col items-center justify-center bg-background gap-y-4 h-full w-full transition-transform group-hover:translate-x-2 group-hover:-translate-y-2 rounded-md"
             >
                 <UserAvatar
                     showBadge
@@ -44,6 +45,15 @@ export const Thumbnail = ({ src, fallback, isLive, username }: ThumbnailProps) =
                 className="rounded-md absolute inset-0 bg-blue-600 opacity-0 group-hover:opacity-100 transition-opacity duration-200 flex items-center justify-center"
             />
             {content}
+            {
+                isLive && (
+                    <div
+                        className="absolute top-2 left-2 group-hover:translate-x-2 group-hover:-translate-y-2 transition-transform"
+                    >
+                        <LiveBadge />
+                    </div>
+                )
+            }
         </div>
     )
 }
