3e2ce2b2d479e2d7e0ac8e38e0dedae3d4c7ac26
DuarteMartinho <duartemartinho11@gmail.com>
2024-01-20 18:04:42 +0000

    31: Added home page and feed service

---
+++ app/(site)/(home)/_components/ResultCard.tsx
@@ -0,0 +1,93 @@
+import { Thumbnail, ThumbnailSkeleton } from "@/components/Thumbnail";
+import { UserAvatar, UserAvatarSkeleton } from "@/components/UserAvatar";
+import { LiveBadge } from "@/components/live-badge";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Stream, User } from "@prisma/client";
+import Link from "next/link";
+
+interface ResultCardProps {
+    data: {
+        id: string;
+        name: string;
+        thumbnailUrl: string | null;
+        isLive: boolean;
+        user: User;
+    };
+}
+
+export const ResultCard = ({ data }: ResultCardProps) => {
+    return (
+        <Link
+            href={`/${data.user.username}`}
+        >
+            <div
+                className="h-full w-full space-y-4"
+            >
+                <Thumbnail
+                    src={data.thumbnailUrl}
+                    fallback={data.user.imageUrl}
+                    isLive={data.isLive}
+                    username={data.user.username}
+                />
+                {
+                    data.isLive && (
+                        <div
+                            className="absolute top-2 left-2 group-hover:translate-x-2 group-hover:-translate-y-2 transition-transform"
+                        >
+                            <LiveBadge />
+                        </div>
+                    )
+                }
+                <div
+                    className="flex gap-x-3"
+                >
+                    <UserAvatar
+                        isLive={data.isLive}
+                        imageUrl={data.user.imageUrl}
+                        username={data.user.username}
+                        showBadge={false}
+                    />
+                    <div
+                        className="flex flex-col text-sm overflow-hidden"
+                    >
+                        <p
+                            className="truncate font-semibold hover:text-blue-500"
+                        >
+                            {data.name}
+                        </p>
+                        <p
+                            className="text-muted-foreground truncate"
+                        >
+                            {data.user.username}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </Link>
+    )
+}
+
+export const ResultCardSkeleton = () => {
+    return (
+        <div
+            className="h-full w-full space-y-4"
+        >
+            <ThumbnailSkeleton />
+            <div
+                className="flex gap-x-3"
+            >
+                <UserAvatarSkeleton />
+                <div
+                    className="flex flex-col gap-y-1"
+                >
+                    <Skeleton
+                        className="h-4 w-32"
+                    />
+                    <Skeleton
+                        className="h-3 w-24"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/(site)/(home)/page.tsx
+++ app/(site)/(home)/page.tsx
@@ -1,10 +1,16 @@
+import { Suspense } from "react";
+import { Results, ResultsSkeleton } from "./_components/Results";
 
 export default function Home() {
   return (
     <div
-      className="flex flex-col gap-y-4"
+      className="h-full p-8 mx-auto"
     >
-      <h1>Home Page</h1>
+      <Suspense
+        fallback={<ResultsSkeleton />}
+      >
+        <Results />
+      </Suspense>
     </div>
   )
 }
